e243e524137b68a1a2d3f50e74474deea72e3106
George-Sava <sava_george.alex@yahoo.com>
2021-07-26 23:33:39 -0700

    Password Generator App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AppTest';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeUseLetters()
+  {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers()
+  {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols()
+  {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(event : Event)
+  {
+    const value= (event.target as HTMLInputElement).value;
+
+    const lengthValue = Number(value);
+    
+    if( Number.isInteger(lengthValue) )
+    {
+      this.length = lengthValue;
+    }
+    
+  }
+
+  onButtonClick()
+  {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnoprstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if( this.includeLetters )
+    {
+      validChars += letters;
+    }
+
+    if( this.includeNumbers )
+    {
+      validChars += numbers;
+    }
+
+    if( this.includeSymbols )
+    {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for(let i =0; i < this.length; i++)
+    {
+      const index = Math.floor(Math.random() * validChars.length);
+
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
 }
